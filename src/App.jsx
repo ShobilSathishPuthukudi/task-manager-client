509aec84cba32ac238a72c833742f0d162031a4a
Shobil Sathish <shobilsathish@Shobils-MacBook-Air.local>
2026-02-01 00:36:14 +0530

    Feat(auth): Implement protected route and handle initial authentication check

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import AuthRoutes from "./routes/AuthRoutes.jsx";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getCurrentUser } from "./features/auth/authThunk.js";
 
-const App = () => (
-  <BrowserRouter>
-    <AuthRoutes />
-  </BrowserRouter>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+  }, [dispatch]);
+
+  return (
+    <BrowserRouter>
+      <AuthRoutes />
+    </BrowserRouter>
+  );
+};
 
 export default App;
